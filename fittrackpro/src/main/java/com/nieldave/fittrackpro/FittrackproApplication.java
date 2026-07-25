9a2d8477fe254826440572d36bfd487dc6e5011a
Niel <nielabhishek6265852@gmail.com>
2026-07-25 21:09:00 +0530

    lets start

---
+++ fittrackpro/src/main/java/com/nieldave/fittrackpro/FittrackproApplication.java
@@ -0,0 +1,13 @@
+package com.nieldave.fittrackpro;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FittrackproApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FittrackproApplication.class, args);
+	}
+
+}
---
+++ fittrackpro/src/test/java/com/nieldave/fittrackpro/FittrackproApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nieldave.fittrackpro;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FittrackproApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
